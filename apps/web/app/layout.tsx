b7bd7f4c3d60f237c0faf83c2b7ad8e15f875ce8
masrafi-000 <smmasrafi000@gmail.com>
2026-06-06 12:35:44 +0600

    feat(ui): add new components and hooks for improved UI functionality
    - Introduced `Separator` component for visual separation in layouts.
    - Added `Sheet` component with triggers, close functionality, and customizable content.
    - Implemented `Sidebar` component with context for state management, mobile responsiveness, and various subcomponents for structure.
    - Created `Skeleton` component for loading states.
    - Added `Sonner` based `Toaster` component for notifications with customizable icons.
    - Implemented `Switch` component for toggle functionality.
    - Added `Tabs` component with triggers and content management.
    - Created `Textarea` component for text input.
    - Introduced `Tooltip` component with customizable content and triggers.
    - Added `useIsMobile` hook for responsive design handling based on screen width.

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,10 +1,11 @@
 import { Geist, Geist_Mono } from "next/font/google"
 
-import "@workspace/ui/globals.css"
 import { ThemeProvider } from "@/components/theme-provider"
-import { cn } from "@workspace/ui/lib/utils";
+import { TooltipProvider } from "@workspace/ui/components/tooltip"
+import "@workspace/ui/globals.css"
+import { cn } from "@workspace/ui/lib/utils"
 
-const geist = Geist({subsets:['latin'],variable:'--font-sans'})
+const geist = Geist({ subsets: ["latin"], variable: "--font-sans" })
 
 const fontMono = Geist_Mono({
   subsets: ["latin"],
@@ -20,10 +21,17 @@ export default function RootLayout({
     <html
       lang="en"
       suppressHydrationWarning
-      className={cn("antialiased", fontMono.variable, "font-sans", geist.variable)}
+      className={cn(
+        "antialiased",
+        fontMono.variable,
+        "font-sans",
+        geist.variable
+      )}
     >
       <body>
-        <ThemeProvider>{children}</ThemeProvider>
+        <ThemeProvider>
+          <TooltipProvider>{children}</TooltipProvider>
+        </ThemeProvider>
       </body>
     </html>
   )
